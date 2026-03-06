70fa33e3ada87a1ebdc737f589fb7904cf530cf9
yezawhlaing <yezawhlaing0@gmail.com>
2026-03-06 22:20:10 +0630

    swagger provided

---
+++ src/main/java/com/backend/api/config/OpenAPIConfig.java
@@ -0,0 +1,19 @@
+package com.backend.api.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenAPIConfig {
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Swagger Documentation for DRMS")
+                        .version("1.0")
+                        .description("API for Disaster Relief Management System in the system"));
+    }
+
+}
