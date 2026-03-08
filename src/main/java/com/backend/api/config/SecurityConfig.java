72cf90f650b27259b9d794890628f1c7fda72298
yezawhlaing <yezawhlaing0@gmail.com>
2026-03-08 15:12:25 +0630

    swagger for permission

--- src/main/java/com/backend/api/config/SecurityConfig.java
+++ src/main/java/com/backend/api/config/SecurityConfig.java
@@ -29,6 +29,12 @@ public class SecurityConfig {
                 .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(
+                                "/swagger-ui/**",
+                                "/swagger-ui.html",
+                                "/v3/api-docs/**",
+                                "/v3/api-docs.yaml"
+                        ).permitAll()
                         .requestMatchers("/api/**").permitAll()
                         .anyRequest().authenticated()
                 );
